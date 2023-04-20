5226014a9a14cb60916fcceab0d2f154fe909e15
jsbyeon <jsbyeon@enliple.com>
2023-04-20 14:02:35 +0900

    어노테이션 생성

---
+++ src/test/java/hello/core/scan/filter/ComponentFilterAppConfigTest.java
@@ -0,0 +1,12 @@
+package hello.core.scan.filter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ComponentFilterAppConfigTest {
+
+    @Test
+    void filterScan() {
+        AnnotationConfigApplicationContext applicationContext = new AnnotationConfigApplicationContext();
+    }
+}

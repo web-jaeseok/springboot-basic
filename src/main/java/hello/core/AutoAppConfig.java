2dde61fb15305cbc72fed7abf8c8fcb65dec24aa
JaeSeok <wotjrl0914@gmail.com>
2023-04-22 10:28:13 +0900

    bean 네이밍 중복시 에러 확인

--- src/main/java/hello/core/AutoAppConfig.java
+++ src/main/java/hello/core/AutoAppConfig.java
@@ -1,5 +1,8 @@
 package hello.core;
 
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.FilterType;
@@ -12,4 +15,10 @@ import org.springframework.context.annotation.FilterType;
 )
 public class AutoAppConfig {
 
+    /* 수동 빈 이름 중복 */
+    /* 수동 빈이 자동 빈을 오버라이딩 하는데 @SpringbootApplication에서 에러로 잡음*/
+//    @Bean(name = "memoryMemberRepository")
+//    MemberRepository memberRepository() {
+//        return new MemoryMemberRepository();
+//    }
 }

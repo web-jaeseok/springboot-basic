8b6de82940e6f6fd30c0833d0c99ecb839b7527f
jsbyeon <jsbyeon@enliple.com>
2023-06-01 13:58:39 +0900

    scope 프로토타입, 싱글톤 동시사용 문제점 확인

--- src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
+++ src/test/java/hello/core/scope/SingletonWithPrototypeTest1.java
@@ -26,11 +26,19 @@ public class SingletonWithPrototypeTest1 {
     @Test
     void singletonClientUserPrototype() {
         AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(PrototypeBean.class);
+
+        ClientBean clientBean1 = ac.getBean(ClientBean.class);
+        int count1 = clientBean1.logic();
+        Assertions.assertThat(count1).isEqualTo(1);
+
+        ClientBean clientBean2 = ac.getBean(ClientBean.class);
+        int count2 = clientBean2.logic();
+        Assertions.assertThat(count2).isEqualTo(2);
     }
 
     @Scope("singleton")
     static class ClientBean {
-        private final PrototypeBean prototypeBean;
+        private final PrototypeBean prototypeBean; // 생성 시점에 주입
 
         @Autowired
         public ClientBean(PrototypeBean prototypeBean) {
@@ -61,7 +69,7 @@ public class SingletonWithPrototypeTest1 {
         }
 
         @PreDestroy
-        public void destory() {
+        public void destroy() {
             System.out.println("PrototypeBen.destroy");
         }
     }
